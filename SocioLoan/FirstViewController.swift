7562afad25ce80b69e940a97d341db6a75e0ddfe
Ben <bendesign555@gmail.com>
2016-06-25 12:34:50 +0200

    Initial Commit

---
+++ SocioLoan/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SocioLoan
+//
+//  Created by Ben Ormos on 25/06/2016.
+//  Copyright © 2016 uk.co.benormos.firstapp. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
